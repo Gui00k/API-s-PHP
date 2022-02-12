7696934170a082f6114af3074bdc49f8f1476ee5
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-02-12 14:49:52 -0300

    Criando tb_deposit e modificando pk do user

--- Default Schema.sql
+++ Default Schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE db_waifu;
 USE db_waifu;
 
 CREATE TABLE tb_users (
-    user_id CHAR(24) PRIMARY KEY,
-    user_addres VARCHAR(249)
+    user_addres CHAR(42) PRIMARY KEY,
+    user_moralis_id CHAR(24),
+    user_balance FLOAT
 );
 
 CREATE TABLE tb_cards (
@@ -16,13 +17,22 @@ CREATE TABLE tb_cards (
 
 CREATE TABLE tb_assets (
     assets_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id CHAR(24) NOT NULL,
+    user_addres CHAR(42),
     card_id INT NOT NULL,
     assets_first_acess DATETIME,
     assets_last_acess DATETIME,
     user_balance FLOAT DEFAULT(0),
+    FOREIGN KEY (user_addres) REFERENCES tb_users(user_addres) ON DELETE CASCADE
     FOREIGN KEY (card_id) REFERENCES tb_cards(card_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES tb_users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE tb_deposit (
+    deposit_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_addres CHAR(42),
+    deposit_hash CHAR(66),
+    deposit_value FLOAT,
+    deposit_date DATETIME,
+    FOREIGN KEY (user_addres) REFERENCES tb_users(user_addres) ON DELETE CASCADE
 );
 
 /* 
