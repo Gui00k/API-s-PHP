5b8729b8edfd6627041d7d6ff0117c2b7be6629b
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-02-16 11:09:03 -0300

    Deletando dados desnecessarios e corrigindo nome de address

--- Default Schema.sql
+++ Default Schema.sql
@@ -3,9 +3,8 @@ CREATE DATABASE db_waifu;
 USE db_waifu;
 
 CREATE TABLE tb_users (
-    user_addres CHAR(42) PRIMARY KEY,
-    user_moralis_id CHAR(24),
-    user_balance FLOAT
+    user_address CHAR(42) PRIMARY KEY,
+    user_balance FLOAT DEFAULT(0)
 );
 
 CREATE TABLE tb_cards (
@@ -17,22 +16,23 @@ CREATE TABLE tb_cards (
 
 CREATE TABLE tb_assets (
     assets_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_addres CHAR(42),
+    user_address CHAR(42),
     card_id INT NOT NULL,
     assets_first_acess DATETIME,
     assets_last_acess DATETIME,
     user_balance FLOAT DEFAULT(0),
-    FOREIGN KEY (user_addres) REFERENCES tb_users(user_addres) ON DELETE CASCADE,
+    FOREIGN KEY (user_address) REFERENCES tb_users(user_address) ON DELETE CASCADE,
     FOREIGN KEY (card_id) REFERENCES tb_cards(card_id) ON DELETE CASCADE
 );
 
 CREATE TABLE tb_deposit (
     deposit_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_addres CHAR(42),
+    user_address CHAR(42),
     deposit_hash CHAR(66),
     deposit_value FLOAT,
     deposit_date DATETIME DEFAULT(NOW()),
-    FOREIGN KEY (user_addres) REFERENCES tb_users(user_addres) ON DELETE CASCADE
+    FOREIGN KEY (user_address) REFERENCES tb_users(user_address) ON DELETE CASCADE,
+    UNIQUE (deposit_hash)
 );
 
 /* 
