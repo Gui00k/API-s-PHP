16e4e80ad6e26db55db7caf0a5ba8247227d9a6e
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-02-21 14:19:12 -0300

    Criando função buy

---
+++ api/buy.php
@@ -0,0 +1,17 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method != 'POST') {
+    http_response_code(404);
+    return;
+} //method not is POST 
+
+header('Content-Type: application/json; charset=utf-8');
+
+//Pegando dados
+$data = json_decode(file_get_contents("php://input"), true);
+$address = @$data['address'];
+
+if (!$address) {
+    echo json_encode(['status' => 'failed1']);
+    return;
+} //No have data
